ecec4a0810d254294b9bf8f054b6a0f89c869910
StevenJohnWeaver <stevenjweaver@gmail.com>
2025-05-29 14:25:09 -0400

    Update variables.tf

--- terraform-configs/variables.tf
+++ terraform-configs/variables.tf
@@ -16,23 +16,3 @@ variable "ssh_public_key_content" { # Use this name if you're passing content di
   type        = string
   sensitive   = true
 }
-
-# provider.tf
-terraform {
-  required_providers {
-    ibm = {
-      source = "IBM-Cloud/ibm"
-    }
-  }
-}
-
-provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = var.region
-}
-
-# main.tf (modify ssh_key resource)
-resource "ibm_is_ssh_key" "ssh_key" {
-  name       = "web-app-ssh-key"
-  public_key = var.ssh_public_key_content # Use the content variable
-}
